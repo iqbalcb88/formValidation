1eaa780ad2aadca3d76d6932fa5d275b4e6ea006
iqbalcb88 <iqbal.cb88@gmail.com>
2022-08-09 11:15:24 +0600

    js done

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
+// check email validation
+function isValidEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
 // Event listener
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -27,6 +36,26 @@ form.addEventListener('submit', function (e) {
   } else {
     console.log('success');
     showSuccess(username);
+  }
+  if (email.value.length < 1 || email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValidEmail(email)) {
+    showError(email, 'Email is not valid');
+  } else {
+    console.log('success');
+    showSuccess(email);
+  }
+  if (password.value.length < 1 || password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    console.log('success');
+    showSuccess(password);
+  }
+  if (password2.value.length < 1 || password2.value === '') {
+    showError(password2, 'Password is required');
+  } else {
+    console.log('success');
+    showSuccess(password2);
   }
 });
 
